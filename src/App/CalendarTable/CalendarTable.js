d6a8f796dda969d541079d3b934dff9f52c9e43d
Daria <dar.zavaliuk@gmail.com>
2023-06-01 21:40:44 +0300

    feat: create CalendarTable component

--- src/App/CalendarTable/CalendarTable.js
+++ src/App/CalendarTable/CalendarTable.js
@@ -1,42 +1,74 @@
 import React, {useState} from 'react';
-import styled from 'styled-components';
-import {ButtonArrow, DeleteButton, SpanMonthTitle, Table, TableWrapper, TBody, TD, TDD, TH, THead, TR} from "./Style";
+import {
+    ButtonArrow,
+    DeleteButton,
+    SpanMonthTitle,
+    Table,
+    TableWrapper,
+    TBody,
+    TD,
+    TDD,
+    TH,
+    THead,
+    TR,
+    TRHead
+} from "./Style/Style";
 import {format} from 'date-fns';
+import DayOfWeekRow from "./Component/DaysOfWeekRow/DaysOfWeekRow";
+import DateTableHeader from "./Component/DateTableHeader/DateTableHeader";
+import WeekNavigation from "./Component/WeekNavigation/WeekNavigation";
 
 function CalendarTable() {
     const dates = [];
     const [selectedItem, setSelectedItem] = useState('');
+    const [active, setActive] = useState(false);
     const [selectedItemTarget, setSelectedItemTarget] = useState('');
+    const [prevColor, setPrevColor] = useState('rgb(255, 255, 255)');
+    const [currentWeek, setCurrentWeek] = useState(new Date());
+    const today = new Date();
+    const firstDayOfWeek = new Date(currentWeek);
+    firstDayOfWeek.setDate(firstDayOfWeek.getDate() - firstDayOfWeek.getDay() + 1);
+    for (let i = 0; i < 7; i++) {
+        const date = new Date(firstDayOfWeek);
+        date.setDate(date.getDate() + i);
+        dates.push(date);
+    }
+    const monthYearString = `${dates[0].toLocaleString('en-GB', {month: 'long'})} ${dates[0].getFullYear()}`;
+    const daysOfWeek = ['', 'M', 'T', 'W', 'T', 'F', 'S', 'S'];
 
     function handleAddEvent(e, date, hour) {
-        e.preventDefault();
+        setActive(true)
         if (selectedItem && format(date, 'yyyy-MM-dd') + (hour < 10 ? ` 0${hour}` : ` ${hour}`))
             console.log(format(date, 'yyyy-MM-dd') + (hour < 10 ? ` 0${hour}` : ` ${hour}`))
-        console.log(e.target)
+        setPrevColor(e.target.style.backgroundColor);
         e.target.style.backgroundColor = '#b8b4fc'
-        e.preventDefault();
         setSelectedItem(format(date, 'yyyy-MM-dd') + (hour < 10 ? ` 0${hour}` : ` ${hour}`))
-        if (selectedItemTarget !== "")
-            selectedItemTarget.style.background = 'white'
-        setSelectedItemTarget(e.target)
-        if (selectedItem) {
-            e.preventDefault();
+
+        if (selectedItemTarget !== "" && e.target !== selectedItemTarget && prevColor === 'rgb(184, 180, 252)') {
+
+        } else if (selectedItemTarget !== "") {
+            selectedItemTarget.style.background = prevColor;
+
         }
+        if (selectedItemTarget !== "" && e.target.style.backgroundColor === "rgb(255, 255, 255)" && selectedItemTarget.style.background === 'rgb(255, 255, 255)')
+            setActive(false)
+
+        setSelectedItemTarget(e.target)
     }
 
     function handleDeleteItem() {
-
         if (selectedItem !== '') {
-            if (selectedItemTarget !== '')
-                selectedItemTarget.style.background = "white"
+            if (selectedItemTarget !== '') {
+                selectedItemTarget.style.background = "rgb(255, 255, 255)"
+                setPrevColor("rgb(255, 255, 255)");
+                setSelectedItem('')
+                setActive(false)
+            }
             let data = JSON.parse(localStorage.getItem('storedData'));
             data = data.filter(el => selectedItem !== el.substring(0, 13))
             localStorage.setItem('storedData', JSON.stringify(data));
         }
-        // tableRef.current.forceUpdate();
     }
-
-    const [currentWeek, setCurrentWeek] = useState(new Date());
 
     function handlePrevWeek() {
         const prevWeek = new Date(currentWeek);
@@ -50,61 +82,21 @@ function CalendarTable() {
         setCurrentWeek(nextWeek);
     }
 
-    const firstDayOfWeek = new Date(currentWeek);
-    firstDayOfWeek.setDate(firstDayOfWeek.getDate() - firstDayOfWeek.getDay());
-    for (let i = 0; i < 7; i++) {
-        const date = new Date(firstDayOfWeek);
-        date.setDate(date.getDate() + i);
-        dates.push(date);
-    }
-    const today = new Date(); // текущая дата
-    const monthYearString = `${dates[0].toLocaleString('en-GB', {month: 'long'})} ${dates[0].getFullYear()}`;
-
     return (
-        <TableWrapper onClick={(e) => e.preventDefault()}>
+        <TableWrapper>
             <Table className="flex-table flex-table-aligned table table-striped table-hover">
                 <THead>
-                    <TR>
-                        <TH></TH>
-                        <TH>S</TH>
-                        <TH>M</TH>
-                        <TH>T</TH>
-                        <TH>W</TH>
-                        <TH>T</TH>
-                        <TH>F</TH>
-                        <TH>S</TH>
-                    </TR>
-                    <TR style={{textAlign: "center"}}>
-                        <TD></TD>
-                        {dates.map((date) => (
-                            <TDD
-                                key={date.toISOString()}
-                                isToday={date.getDate() === today.getDate()}
-                            >
-                                {date.getDate()}
-                            </TDD>
-                        ))}
-                    </TR>
-                    <TR style={{textAlign: "center"}}>
-                        <TD></TD>
-                        <TD>
-                            <ButtonArrow onClick={handlePrevWeek}>{"<"}</ButtonArrow>
-                        </TD>
-                        <TD></TD>
-                        <TD></TD>
-                        <TD>
-                            <SpanMonthTitle>{monthYearString}</SpanMonthTitle>
-                        </TD>
-                        <TD></TD>
-                        <TD></TD>
-                        <TD>
-                            <ButtonArrow onClick={handleNextWeek}>{">"}</ButtonArrow>
-                        </TD>
-                    </TR>
+                    <DayOfWeekRow daysOfWeek={daysOfWeek}/>
+                    <DateTableHeader dates={dates}/>
+                    <WeekNavigation
+                        monthYearString={monthYearString}
+                        handlePrevWeek={handlePrevWeek}
+                        handleNextWeek={handleNextWeek}
+                    />
                 </THead>
-                <TBody className="scroll" onClick={(e) => e.preventDefault()}>
+                <TBody className="scroll" >
                     {[...Array(24)].map((_, hour) => (
-                        <TR key={hour} onClick={(e) => e.preventDefault()}>
+                        <TR key={hour}>
                             <TD>
                                 <span style={{color: "#bcbcbc"}}>{`${hour}:00`}</span>
                             </TD>
@@ -137,7 +129,7 @@ function CalendarTable() {
                     ))}
                 </TBody>
             </Table>
-            {selectedItem &&
+            {active &&
                 <DeleteButton onClick={handleDeleteItem}>Delete</DeleteButton>
             }
         </TableWrapper>
