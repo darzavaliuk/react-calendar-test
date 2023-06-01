548d6d0229ef541dd81a7fc4be9f67ec24b3ed1c
Daria <dar.zavaliuk@gmail.com>
2023-06-01 21:43:40 +0300

    fix: change start styles and fonts

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from './App/App';
+import GlobalStyle from './Style/GlobalStyle';
+import GlobalFonts from "./Fonts/GlobalFonts";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <GlobalFonts/>
+        <GlobalStyle />
+        <App/>
+    </React.StrictMode>
 );
